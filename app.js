d5d70859bea6620017d9b2a8299b23f32ceeaffa
Jordan R <jaygerzero@gmail.com>
2019-12-01 17:33:26 -0500

    vanilla DOM manipulation

--- app.js
+++ app.js
@@ -18,6 +18,10 @@ activePlayer = 0;
 dice = Math.floor(Math.random() * 6 + 1);
 console.log(dice);
 
+// document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>'; 
 document.querySelector('#current-' + activePlayer).textContent = dice;
+var x = document.querySelector('#score-0').textContent;
+console.log(x);
 
+document.querySelector('.dice').style.display = 'none';
  
