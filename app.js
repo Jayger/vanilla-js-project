12c7400ca91f9f2bebf1a4b86ce4f4d828d7b0ef
Jordan R <jaygerzero@gmail.com>
2019-12-01 18:10:37 -0500

    updating score

--- app.js
+++ app.js
@@ -31,6 +31,18 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
     diceDOM.src = 'dice-' + dice + '.png';
 
     // 3. Update the round score IF the rolled number was NOT a 1
+    if(dice !==1) {
+        // Add score
+        roundScore += dice;
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    } else {
+        // Next Player
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        roundScore = 0;
+
+        document.getElementById('current-0').textContent = '0';
+        document.getElementById('current-1').textContent = '0';   
+    }
 
 }); 
  
